bea24b37a0c8c193537b54ed66b1f9c3838bda05
TiagoBarbosa88 <sombr43@gmail.com>
2023-03-01 22:31:34 -0300

    create date,types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import React from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { categories } from "./data/categories";
+import { items } from "./data/items";
+import { getCurrentMonth } from "./helpers/dateFilter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+	const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+	return (
+		<C.Container>
+			<C.Header>
+				<C.HeaderText>Finances</C.HeaderText>
+			</C.Header>
+			<C.Body>
+				{/* Área de informações */}
+
+				{/* Área de inserção */}
+
+				{/* Área de itens */}
+			</C.Body>
+		</C.Container>
+	);
+};
 
 export default App;
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  { date: new Date(2023, 4, 10),  category: 'food', title: 'McDonalds', value: 32.10 },
+  { date: new Date(2023, 4, 11),  category: 'food', title: 'Burger King', value: 40.00 },
+  { date: new Date(2023, 4, 15),  category: 'energy', title: 'Enel', value: 150 },
+  { date: new Date(2023, 5, 17),  category: 'salary', title: 'Acme', value: 5000 }
+];
